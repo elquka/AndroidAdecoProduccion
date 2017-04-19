289cbe4dd3f90cab668a68f81412834fdfcac060
elquka <jonathanloker@gmail.com>
2017-04-19 18:13:45 -0300

    Pablito

--- app/src/main/java/com/example/jloker/adecoproduccion/MainActivity.java
+++ app/src/main/java/com/example/jloker/adecoproduccion/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.jloker.adecoproduccion;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
